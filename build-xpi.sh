76e9467a59bcf28dc1600522290c16fc0b5340e1
cfinke <cfinke@gmail.com>
2008-05-24 13:36:23 +0000

    Updated for Firefox 3: version 1.1.3.

--- build-xpi.sh
+++ build-xpi.sh
@@ -7,6 +7,7 @@ chmod -R 0777 links-like-this/
 
 mkdir .tmp_xpi_dir/
 cp -r links-like-this/* .tmp_xpi_dir/
+rm -rf `find ./.tmp_xpi_dir/ -name ".svn"`
 
 cd .tmp_xpi_dir/chrome/
 zip -rq ../links-like-this.jar *
@@ -16,3 +17,5 @@ cd ../
 zip -rq ../links-like-this.xpi *
 cd ../
 rm -rf .tmp_xpi_dir/
+
+cp links-like-this.xpi ~/Desktop/
